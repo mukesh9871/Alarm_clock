b2f0ec927b44ad3cfbb5aa444d655faf962b4ca2
Mukesh Kumar <mukesh9871@gmail.com>
2017-06-19 13:47:23 +0530

    Woriking Alarm without sound.

--- first.cpp
+++ first.cpp
@@ -10,7 +10,7 @@ using namespace std;
 static int alarm_index =0;
 static int alarm_hour,alarm_min;
 pthread_cond_t cond;
-
+pthread_mutex_t mutex;
 void sleep(int x)
 {
 	Sleep(x);
@@ -23,19 +23,6 @@ void clear_line(int n)
 }
 void *printTime(void *arg)
 {
-	if(alarm_index==0){
-		cout<<"Do you wany to set alarm?"<<endl;
-		pthread_cond_wait(&cond,0);
-		if(getchar()=='y'||getchar()=='Y'){
-			cout<<"Enter alarm time in Hour:";
-			cin>>alarm_hour;
-			cin.ignore();
-			cout<<"Enter alarm time in Minute:";
-			cin>>alarm_min;
-			alarm_index=1;
-		}
-		pthread_cond_signal(&cond);
-	}		
 	tm *ltm;
 	do{
 		time_t tm =time(0);
@@ -45,7 +32,11 @@ void *printTime(void *arg)
 			if(alarm_hour == ltm->tm_hour && alarm_min == ltm->tm_min){
 				cout<<"BOOOOOOOOOOOOOM Wake up"<<endl;
 				cout<<"						"<<ltm->tm_hour<<":"<<ltm->tm_min<<":"<<ltm->tm_sec<<endl;
+				alarm_index=0;
+				exit(0);
 			}
+			else
+				cout<<"						"<<ltm->tm_hour<<":"<<ltm->tm_min<<":"<<ltm->tm_sec<<endl;
 		}
 		else
 			cout<<"						"<<ltm->tm_hour<<":"<<ltm->tm_min<<":"<<ltm->tm_sec<<endl;
@@ -62,14 +53,26 @@ int main()
 {
 	pthread_t t1;
 	pthread_t t2;
-	pthread_cond_init(&cond,0);
+
+	if(alarm_index==0){
+		cout<<"Do you wany to set alarm?"<<endl;
+		char c = getchar();
+		cin.ignore();
+		if(c=='y'||c=='Y'){
+			cout<<"Enter alarm time in Hour:";
+			cin>>alarm_hour;
+			cin.ignore();
+			cout<<"Enter alarm time in Minute:";
+			cin>>alarm_min;
+			cin.ignore();
+			alarm_index=1;
+		}
+	}
 	pthread_create(&t1,0,&printTime,NULL);
 	pthread_create(&t2,0,&cargRet,NULL);
 
 	pthread_join(t1,0);
 	pthread_join(t2,0);
-	
-	pthread_cond_destroy(&cond);
 	
 	return 0;
 }
